d165461105fd129d883f52a0492d588f053a93f5
Ander <[AndersonLee0248@gmail.com]>
2024-11-18 17:22:29 -0800

    Added reccomendations for searchbar

--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -1,36 +1,19 @@
-import { useNavigate } from 'react-router-dom';
 import '../styles/HomeScreen.css'
+import SearchBar from '../components/SearchBar';
+import { useNavigate } from 'react-router-dom';
 
-import React, { useState } from 'react';
 
 const HomeScreen = () =>{
-    const [searchedChampion, setSearchedChampion] = useState(''); // New state to store searched champion
+    
     const navigate = useNavigate();
 
-    const handleInputChange = (event) => {
-        setSearchedChampion(event.target.value); // Update the input value
-    };
-
-    const handleSearch = () => {
-        if(searchedChampion.trim() !== ''){
-            navigate(`/ChampInfo/${searchedChampion}`)
-        }
+    const handleSelectChampion = (champion) => {
+        navigate(`/ChampInfo/${champion}`);
     };
 
     return (
-        <div className = "container">
-            <h1 className = "title">LoLHelp</h1>
-            <div className = "searchbar">
-                <input 
-                    className = "inputForm"
-                    type="text" 
-                    placeholder="Enter Champion Name" 
-                    value={searchedChampion} 
-                    onChange={handleInputChange} 
-                />
-                <button className = "searchButton" onClick={handleSearch}></button>
-            </div>
-        </div>
+        
+        <SearchBar onSelectChampion = {handleSelectChampion}/>
     );
 }
 
---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,73 @@
+import '../styles/HomeScreen.css'
+import championNames from './formattedChampionNames';
+import React, { useState } from 'react';
+
+const SearchBar = ({onSelectChampion}) =>{
+    const [searchedChampion, setSearchedChampion] = useState(''); // New state to store searched champion
+    const [suggestions,setSuggestions] = useState([])
+
+    const handleInputChange = (e) => {
+        const input = e.target.value;
+        setSearchedChampion(input);
+
+        //Check if input is empty so we can clear suggestions
+        if(input === ''){
+            setSuggestions([]);
+        }
+        else{
+            // Filter champions based on the input text
+            const filteredChampions = championNames.filter((champion) =>
+                champion.toLowerCase().includes(input.toLowerCase())
+            );
+            // Show up to 5 suggestions
+            setSuggestions(filteredChampions.slice(0, 5));
+        }
+    };
+
+    const handleSuggestionClick = (champion) => {
+        setSearchedChampion(champion);
+        setSuggestions([]);
+        onSelectChampion(champion);
+    };
+
+
+    return (
+        <div className = "container">
+            <h1 className = "title">LoLHelp</h1>
+            <div className = "searchbar">
+                <input 
+                    className = "inputForm"
+                    type="text" 
+                    placeholder="Enter Champion Name" 
+                    value={searchedChampion} 
+                    onChange={handleInputChange} 
+                />
+                
+                <button className = "searchButton" onClick={()=>onSelectChampion(searchedChampion)}></button>
+            </div>
+            <div className = "suggestion-container">
+                {suggestions.length > 0 && (
+                    <ul className="suggestions-list">
+                        {suggestions.map((champion, index) => (
+                            <li 
+                                key={index} 
+                                onClick={() => handleSuggestionClick(champion)}
+                                className="suggestion-item"
+                            >
+                                
+                                <img 
+                                src = {"https://ddragon.leagueoflegends.com/cdn/14.22.1/img/champion/" + champion +".png"}
+                                alt = "Failed to load profile of champion" //Show profile picture as well
+                                className='list-profilePic'
+                                ></img>
+                                {champion}
+                            </li>
+                        ))}
+                    </ul>
+                    )}
+            </div>
+        </div>
+    );
+}
+
+export default SearchBar
